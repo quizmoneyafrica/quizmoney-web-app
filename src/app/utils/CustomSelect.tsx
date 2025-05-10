2c07ff940de973c135a60684289ac405d2b92839
morakinyo <olamidemoraks@gmail.com>
2025-05-10 16:21:34 +0100

    style(remove dark bg from input)

--- src/app/utils/CustomSelect.tsx
+++ src/app/utils/CustomSelect.tsx
@@ -53,7 +53,7 @@ export default function CustomSelect({
           disabled={disabled}
           required={required}
           className={cn(
-            "w-full appearance-none rounded-[6px] caret-primary-500 border border-neutral-300 px-4 py-3 pr-12 text-base focus:outline-none focus:ring-1 focus:ring-primary-500 focus:border-primary-500 disabled:bg-neutral-100 dark:disabled:bg-neutral-900 disabled:cursor-not-allowed",
+            "w-full appearance-none rounded-[6px] caret-primary-500 border border-neutral-300 px-4 py-3 pr-12 text-base focus:outline-none focus:ring-1 focus:ring-primary-500 focus:border-primary-500 disabled:bg-neutral-100  disabled:cursor-not-allowed",
             className
           )}
         >
