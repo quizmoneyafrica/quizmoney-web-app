3f27ccc012ae92a380dc65a0825c5545296bac53
Solomonwole <solomonwolemail@gmail.com>
2025-05-12 08:55:27 -0400

    updated logout in settings

--- src/app/(screens)/(protected)/(tabs)/settings/page.tsx
+++ src/app/(screens)/(protected)/(tabs)/settings/page.tsx
@@ -6,9 +6,11 @@ import { Flex, Grid } from "@radix-ui/themes";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function Page() {
 	const router = useRouter();
+	const [openLogout, setOpenLogout] = useState(false);
 	return (
 		<motion.div
 			initial={{ opacity: 0, y: 10 }}
@@ -113,28 +115,28 @@ function Page() {
 							<ChevronRightIcon height={25} width={25} />
 						</Flex>
 
-						<LogoutDialog>
-							<Flex
-								className=" bg-white p-4 md:p-6 rounded-2xl sm:rounded-xl border border-zinc-200"
-								align={"center"}
-								justify={"between"}>
-								<Flex gap={"3"} align={"center"}>
-									<div className="h-12 w-12 bg-rose-50 rounded-full flex justify-center items-center">
-										<Image
-											src="/icons/logout.svg"
-											alt="terms"
-											width={24}
-											height={24}
-										/>
-									</div>
-									<p className=" text-lg font-semibold ">Logout</p>
-								</Flex>
-								<ChevronRightIcon height={25} width={25} />
+						<Flex
+							onClick={() => setOpenLogout(true)}
+							className=" bg-white p-4 md:p-6 rounded-2xl sm:rounded-xl border border-zinc-200"
+							align={"center"}
+							justify={"between"}>
+							<Flex gap={"3"} align={"center"}>
+								<div className="h-12 w-12 bg-rose-50 rounded-full flex justify-center items-center">
+									<Image
+										src="/icons/logout.svg"
+										alt="terms"
+										width={24}
+										height={24}
+									/>
+								</div>
+								<p className=" text-lg font-semibold ">Logout</p>
 							</Flex>
-						</LogoutDialog>
+							<ChevronRightIcon height={25} width={25} />
+						</Flex>
 					</Grid>
 				</Flex>
 			</Flex>
+			<LogoutDialog open={openLogout} onOpenChange={setOpenLogout} />
 		</motion.div>
 	);
 }
