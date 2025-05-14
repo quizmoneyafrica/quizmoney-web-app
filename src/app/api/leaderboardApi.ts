9325bb2198872a669886b6fe4d1cbf355506bf7d
morakinyo <olamidemoraks@gmail.com>
2025-05-14 21:40:47 +0100

    implement api for leaderboard and profile

--- src/app/api/leaderboardApi.ts
+++ src/app/api/leaderboardApi.ts
@@ -4,14 +4,22 @@ import { ApiResponse } from "./interface";
 
 const LeaderboardAPI = {
   getAllTimeLeaderboard(): Promise<AxiosResponse<ApiResponse>> {
-    return axios.post(`${BASE_URL}/getAllTimeLeaderboard`, {
-      headers: getSessionTokenHeaders(),
-    });
+    return axios.post(
+      `${BASE_URL}/getAllTimeLeaderboard`,
+      {},
+      {
+        headers: getSessionTokenHeaders(),
+      }
+    );
   },
   getLastGameLeaderboard(): Promise<AxiosResponse<ApiResponse>> {
-    return axios.post(`${BASE_URL}/getLastGameLeaderboard`, {
-      headers: getSessionTokenHeaders(),
-    });
+    return axios.post(
+      `${BASE_URL}/getLastGameLeaderboard`,
+      {},
+      {
+        headers: getSessionTokenHeaders(),
+      }
+    );
   },
 };
 
