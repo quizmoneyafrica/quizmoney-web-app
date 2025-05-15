70ce5a6fd7cf73c3672788d740139ec307eea66f
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-05-15 17:09:15 +0100

    Update WalletLayout.tsx

--- src/app/components/wallet/WalletLayout.tsx
+++ src/app/components/wallet/WalletLayout.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import TransactionHistory from "./TransactionHistory";
 import WithdrawalAccounts from "./WithdrawalAccounts";
+import { getAuthUser } from "@/app/api/userApi";
 
 export default function WalletLayout() {
+  const user = getAuthUser();
+  console.log("====================================");
+  console.log(JSON.stringify(user, null, 2));
+  console.log("====================================");
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 pb-28 md:pb-0 gap-4">
       <div className="col-span-1 md:col-span-3">
