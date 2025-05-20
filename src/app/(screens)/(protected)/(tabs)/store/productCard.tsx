195a11b5f197c8f267fac9dd932b083611932b7b
morakinyo <olamidemoraks@gmail.com>
2025-05-20 13:07:53 +0100

    feat(store): update store ui

--- src/app/(screens)/(protected)/(tabs)/store/productCard.tsx
+++ src/app/(screens)/(protected)/(tabs)/store/productCard.tsx
@@ -10,13 +10,62 @@ import { useRouter } from "next/navigation";
 import CustomButton from "@/app/utils/CustomBtn";
 import { encryptData } from "@/app/utils/crypto";
 import { useAuth } from "@/app/hooks/useAuth";
-const ProductCard = ({ product }: { product: Product }) => {
+import { Eraser } from "@/app/icons/icons";
+const ProductCard = ({
+  product,
+  index,
+}: {
+  product: Product;
+  index: number;
+}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const { loginUser } = useAuth();
+
+  const displayColor = [
+    {
+      bg: "bg-[#ECF6FD]",
+      text: "text-[#2A75BC]",
+      button: "!bg-[#17478B]",
+      border: "border-[#2980D6]",
+    },
+    {
+      bg: "bg-[#E7FEED]",
+      text: "text-[#00C449]",
+      button: "!bg-[#009028]",
+      border: "border-[#00B23D]",
+    },
+    {
+      bg: "bg-[#F6E4F6]",
+      text: "text-[#85119F]",
+      button: "!bg-[#85119F]",
+      border: "border-[#9817A6]",
+    },
+    {
+      bg: "bg-[#FFEAEE]",
+      text: "text-[#DE1528]",
+      button: "!bg-[#C30012]",
+      border: "border-[#DE1528]",
+    },
+    {
+      bg: "bg-[#DFF9FF]",
+      text: "text-[#00BBE3]",
+      button: "!bg-[#00BBE3]",
+      border: "border-[#00BBE3]",
+    },
+    {
+      bg: "bg-[#FFFCE7]",
+      text: "text-[#F8B93C]",
+      button: "!bg-[#F8B93C]",
+      border: "border-[#F4A235]",
+    },
+  ];
+
+  console.log(6 % (6 - index), index);
+  // console.log(displayColor[index % displayColor.length]);
 
   const handlePurchase = async () => {
     setIsLoading(true);
@@ -41,114 +90,139 @@ const ProductCard = ({ product }: { product: Product }) => {
       });
   };
   return (
-    <div className=" h-[187px] md:h-[217px] bg-white border-zinc-200 border rounded-3xl p-8">
-      <Flex justify="between" className=" h-full" align="center">
-        <Flex direction="column" justify="between" gap="2" className=" h-full">
-          <p className=" text-lg md:text-2xl font-bold">
-            {product?.productName ?? "Eraser"}
-          </p>
-          <p className=" text-sm md:text-base">
-            {product?.productDescription ??
-              "Let you correct wrong answers in the game."}
-          </p>
+    <>
+      <div
+        className={` relative min-h-[265px] md:min-h-[289px] ${
+          displayColor[index % displayColor.length].bg
+        } ${
+          displayColor[index % displayColor.length].border
+        } border-2 rounded-3xl py-4`}
+      >
+        <Eraser
+          className={` absolute top-3 right-0 ${
+            displayColor[index % displayColor.length].text
+          } opacity-10`}
+        />
 
-          <CustomButton
-            onClick={() => setIsOpen(true)}
-            className="!px-6 !py-2 md:!py-3 w-fit"
-          >
-            <div className="flex items-center gap-2 text-white font-semibold ">
-              Buy <ArrowRightIcon />
-            </div>
-          </CustomButton>
-        </Flex>
-        <Flex
-          height={{ md: "107px", initial: "90px" }}
-          width={{ md: "105px", initial: "92px" }}
-          className="rounded-xl bg-zinc-100"
-          direction="column"
-        >
-          <div className="w-full flex-1 h-full flex flex-row justify-center items-center gap-2">
-            <Image
-              src="/icons/eraser.svg"
-              alt="eraser"
-              height={100}
-              width={100}
-              className="w-6 h-6 scale-150 border"
-            />
-            <p>x{product?.productQuantity ?? "2"}</p>
-          </div>
-          <div className="w-full h-[33px] -space-x-5 font-bold flex-[.5] overflow-hidden flex flex-row justify-center items-center gap-2 bg-primary-900 text-white rounded-b-xl">
-            <Image
-              src="/assets/images/chest.svg"
-              alt="chest"
-              height={1000}
-              width={1000}
-              className="w-10 h-10 scale-150 border"
-            />
-
-            <p className=" text-sm sm:text-lg font-bold pr-2">
-              ₦{product?.productPrice}
+        <div className=" flex flex-col justify-between w-full h-full ">
+          <div className=" px-4 z-10">
+            <p
+              className={`${
+                displayColor[index % displayColor.length].text
+              } md:text-[28px] text-[24px] font-bold`}
+            >
+              {product?.productName}
             </p>
+            <p className=" font-semibold">{product?.productDescription}</p>
           </div>
-        </Flex>
-      </Flex>
+          <div className="flex flow-row items-center my-4">
+            <div
+              className={` w-[20%] h-[1px] ${
+                displayColor[index % displayColor.length].border
+              } border-t`}
+            />
+
+            <div
+              className={`bg-white ${
+                displayColor[index % displayColor.length].border
+              } border w-full rounded-2xl py-4 flex-1 flex flex-col items-center justify-center gap-2`}
+            >
+              <div className="flex-row flex  items-center">
+                <p className=" text-2xl  font-semibold">
+                  ₦{product?.productPrice.toLocaleString()}{" "}
+                </p>
+                <span className="font-semibold inline-flex pl-1">
+                  / {product?.productQuantity} Erasers
+                </span>
+              </div>
+
+              <p className=" bg-[#C4FBD2] text-[#009028] text-xs px-2 rounded-full">
+                <span className=" font-semibold">
+                  {product?.bonus > 0 ? product?.bonus : "No"}{" "}
+                </span>
+                Bonus Eraser
+              </p>
+            </div>
+
+            <div
+              className={` w-[20%] h-[1px] ${
+                displayColor[index % displayColor.length].border
+              } border-t`}
+            />
+          </div>
+          <div className="px-4">
+            <CustomButton
+              onClick={() => setIsOpen(true)}
+              className={`!px-6 !py-2 md:!py-3 w-full justify-center flex  ${
+                displayColor[index % displayColor.length].button
+              }`}
+            >
+              <div
+                className={`flex items-center gap-2 text-white font-semibold `}
+              >
+                Buy Eraser <ArrowRightIcon />
+              </div>
+            </CustomButton>
+          </div>
+        </div>
+      </div>
       <Modal
         open={isOpen}
         onOpenChange={setIsOpen}
         className=" !w-[1000px]"
         title="Verify Purchase"
-        description={`Buy ${product?.productQuantity ?? "2"} ${
-          product?.productName ?? "Eraser"
-        }`}
+        description={``}
       >
         <Flex
           direction={"column"}
           gap="2"
           justify={"between"}
-          className=" min-h-[200px] "
+          className=" min-h-[230px] w-full  "
         >
           <Flex
             gap="2"
-            className="w-full border border-zinc-200 rounded-2xl p-4 mt-4"
+            className="w-full border border-primary-500 rounded-2xl p-3 py-6 md:px-6 mt-4"
             justify={"between"}
+            align={"end"}
           >
-            <div className="flex gap-2 items-center">
-              <Image
-                src="/icons/eraser.svg"
-                alt="eraser"
-                height={100}
-                width={100}
-                className="w-6 h-6 scale-150 border"
-              />
-              <p>x{product?.productQuantity ?? "2"}</p>
-              <p className="text-lg font-bold ">
-                {product?.productName ?? "Eraser"}
-              </p>
+            <div className=" space-y-3">
+              <p className="font-bold ">{product?.productName}</p>
+              <div className="flex gap-2 items-center">
+                <Image
+                  src="/icons/eraser.svg"
+                  alt="eraser"
+                  height={100}
+                  width={100}
+                  className="w-6 h-6 scale-150 border"
+                />
+                <p className=" text-blue-500">
+                  {product?.productQuantity ?? "2"} Eraser
+                </p>
+                {product?.bonus > 0 && (
+                  <p className=" text-[#00B23D] font-semibold">
+                    {" "}
+                    <span className=" font-semibold text-black">+</span>{" "}
+                    {product?.bonus} Bonus
+                  </p>
+                )}
+              </div>
             </div>
 
-            <div className="w-fit h-[33px] px-4 -space-x-5 font-bold  overflow-hidden flex flex-row justify-center items-center gap-2 bg-primary-900 text-white rounded-2xl">
-              <Image
-                src="/assets/images/chest.svg"
-                alt="chest"
-                height={1000}
-                width={1000}
-                className="w-10 h-10 scale-150 border"
-              />
-
+            <div className="w-fit h-[33px] px-4 -space-x-5 font-bold  overflow-hidden flex flex-row justify-center items-center gap-2 bg-primary-50 text-black rounded-md">
               <p className=" text-sm sm:text-base font-bold pr-2">
                 ₦{product?.productPrice ?? "100"}
               </p>
             </div>
           </Flex>
-
-          <CustomButton
-            onClick={handlePurchase}
-            loader={isLoading}
-            disabled={isLoading}
-          >
-            Proceed to Pay ₦{product?.productPrice}
-          </CustomButton>
         </Flex>
+        <CustomButton
+          onClick={handlePurchase}
+          loader={isLoading}
+          disabled={isLoading}
+          className=" w-full"
+        >
+          Proceed to Pay ₦{product?.productPrice}
+        </CustomButton>
       </Modal>
       <SuccessMessageModal
         open={isSuccess}
@@ -168,7 +242,7 @@ const ProductCard = ({ product }: { product: Product }) => {
         onClose={() => router.push("/wallet")}
         actionLabel="Fund Account"
       />
-    </div>
+    </>
   );
 };
 
