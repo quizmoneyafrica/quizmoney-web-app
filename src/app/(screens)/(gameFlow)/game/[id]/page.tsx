1c89ad6956960a31650b1591bc97143033c4524a
Solomon Adewole <solomonwolemail@gmail.com>
2025-05-25 09:12:47 +0100

    mend

--- src/app/(screens)/(gameFlow)/game/[id]/page.tsx
+++ src/app/(screens)/(gameFlow)/game/[id]/page.tsx
@@ -1,11 +1,5 @@
 import ClientPage from "./clientPage";
 
-function Page({ params }: { params: { id: string } }) {
-  return (
-    <>
-      <ClientPage gameId={params.id} />
-    </>
-  );
+export default async function Page({ params }: { params: { id: string } }) {
+  return <ClientPage gameId={params.id} />;
 }
-
-export default Page;
