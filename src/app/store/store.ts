e2c22d3c104328b221c20d7e4993bfc10e601d15
Solomon Adewole <solomonwolemail@gmail.com>
2025-05-26 05:55:05 +0100

    stored

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -4,6 +4,7 @@ import walletReducer from "./walletSlice";
 import gameReducer from "./gameSlice";
 import demoReducer from "./demoSlice";
 import notificationReducer from "./notificationSlice";
+import { createFilter } from "redux-persist-transform-filter";
 
 import {
   persistStore,
@@ -18,12 +19,20 @@ import {
 // import storage from "redux-persist/lib/storage";
 import localForage from "localforage";
 
+const authTransform = createFilter("auth", ["userEncryptedData"]);
+
+const authPersistConfig = {
+  key: "auth",
+  storage: localForage,
+  transforms: [authTransform],
+};
+
 const persistConfig = {
   key: "root",
   storage: localForage,
 };
 
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 const persistedWalletReducer = persistReducer(persistConfig, walletReducer);
 
 export const store = configureStore({
