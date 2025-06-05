2cd273310a98559454d0f30cb84c63d36045e690
QM Technologies <160077684+qmtechnologiesafrica@users.noreply.github.com>
2025-06-05 17:36:53 +0100

    Update splash.tsx

--- src/app/components/splashScreen/splash.tsx
+++ src/app/components/splashScreen/splash.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { NlrcIcon } from "@/app/icons/icons";
+import { Text } from "@radix-ui/themes";
 
 export default function Splash() {
   const router = useRouter();
