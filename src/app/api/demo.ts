964da20c400d71e7a4c57acc165205c4de32a4cc
QM Technologies <160077684+qmtechnologiesafrica@users.noreply.github.com>
2025-06-08 18:56:30 +0100

    Update demo.ts

--- src/app/api/demo.ts
+++ src/app/api/demo.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { appHeaders, BASE_URL } from "./userApi";
 import { ApiResponse } from "./interface";
-
+ 
 const DemoApi = {
   fetchDemoGame(): Promise<AxiosResponse<ApiResponse>> {
     return axios.post(`${BASE_URL}/getPracticeQuestions`, {}, { headers: appHeaders });
