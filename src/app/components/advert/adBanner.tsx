183f314ab173531ea96ee133cce2e582dab072a2
Solomon Adewole <solomonwolemail@gmail.com>
2025-06-20 08:31:40 +0100

    ad banner, social links, how to install

---
+++ src/app/components/advert/adBanner.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import React from "react";
+
+function AdBanner() {
+  return (
+    <div className="">
+      <Image
+        src="/assets/images/ad-banner.png"
+        alt="Quiz Money Ad Space"
+        width={758}
+        height={154}
+        priority
+        className="w-full h-full object-contain rounded-xl"
+      />
+    </div>
+  );
+}
+
+export default AdBanner;
