453a5029c4280a877b0642ad38ad7c9872da4199
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-06-26 18:57:20 +0100

    core: update withdrawal pin reset

--- src/app/components/wallet/WalletBalance.tsx
+++ src/app/components/wallet/WalletBalance.tsx
@@ -125,7 +125,7 @@ export default function WalletBalance() {
             </QmDrawer>
 
             <QmDrawer
-              open={withdrawalModal}
+              open={false}
               onOpenChange={(val) => store.dispatch(setWithdrawalModal(val))}
               title="Withdraw"
               titleLeft
@@ -151,17 +151,13 @@ export default function WalletBalance() {
                     wallet?.bankAccounts &&
                     wallet?.bankAccounts.length >= 3
                   ) {
-                    toast.info(
-                      "You've already have three account number listed",
-                      {
-                        position: "top-right",
-                      }
-                    );
+                    toast.info("You already have three account number listed", {
+                      position: "top-right",
+                    });
 
                     return;
                   }
                   store.dispatch(setWithdrawalModal(false));
-
                   store.dispatch(setAddBankModal(true));
                 }}
               />
