93bed8ac2dfc44338fcedd0d5cb7acd5e8150c9d
Solomon Adewole <solomonwolemail@gmail.com>
2025-06-27 09:38:32 +0100

    update privacy policy

---
+++ src/app/components/privacyPolicyUpdate/privacyPolicyUpdate.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React, { useEffect } from "react";
+import { useRef, useState } from "react";
+import QmDrawer from "../drawer/drawer";
+import CustomButton from "@/app/utils/CustomBtn";
+
+function PrivacyPolicyUpdate() {
+  const [htmlContent, setHtmlContent] = useState("");
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [hasScrolledToEnd, setHasScrolledToEnd] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const accepted = localStorage.getItem("acceptedPrivacyPolicy") === "true";
+    if (!accepted) {
+      setOpen(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetch("/privacy-policy.html")
+      .then((res) => res.text())
+      .then((html) => setHtmlContent(html));
+  }, []);
+
+  const handleScroll = () => {
+    const el = contentRef.current;
+    if (el && el.scrollHeight - el.scrollTop === el.clientHeight) {
+      setHasScrolledToEnd(true);
+    }
+  };
+
+  const onAccept = () => {
+    localStorage.setItem("acceptedPrivacyPolicy", "true");
+    setOpen(false);
+  };
+  return (
+    <QmDrawer
+      open={open}
+      onOpenChange={setOpen}
+      dismissible={false}
+      hideCloseBtn
+      heightClass="md:h-[85%]"
+      title="Updated Privacy Policy"
+      titleLeft
+      background="bg-primary-50"
+    >
+      <div>
+        <div className="bg-primary-100 p-4 rounded-lg overflow-y-scroll max-h-[80%] space-y-4">
+          <p className="font-bold">QM Technologies &quot;Quiz Money&quot;</p>
+          <hr />
+          <div
+            ref={contentRef}
+            onScroll={handleScroll}
+            className="h-[50dvh] overflow-y-auto p-4 bg-primary-50 rounded-lg text-sm text-[#0f0f1a] space-y-4"
+          >
+            <div
+              className="prose max-w-none text-sm space-y-4"
+              dangerouslySetInnerHTML={{ __html: htmlContent }}
+            />
+          </div>
+        </div>
+        <div className="flex justify-end mt-6">
+          <CustomButton
+            width="full"
+            size="lg"
+            onClick={onAccept}
+            disabled={!hasScrolledToEnd}
+            className={`${!hasScrolledToEnd && "cursor-not-allowed"}`}
+          >
+            Accept Terms & Privacy Policy
+          </CustomButton>
+        </div>
+      </div>
+    </QmDrawer>
+  );
+}
+
+export default PrivacyPolicyUpdate;
