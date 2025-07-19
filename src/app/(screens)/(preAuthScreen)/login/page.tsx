93152e94d228bddc642bdfef2dfcb52aca98304e
Victor Aiyeola <aiyeolavictor@gmail.com>
2025-07-19 08:19:06 +0100

    Refactor LoginPage component structure and update Tailwind CSS configuration

--- src/app/(screens)/(preAuthScreen)/login/page.tsx
+++ src/app/(screens)/(preAuthScreen)/login/page.tsx
@@ -6,37 +6,37 @@ import LoginForm from "./loginForm";
 import Image from "next/image";
 
 const LoginPage = () => {
-	const [loading, setLoading] = useState(false);
-	
-	return (
-		<>
-			<Grid columns={{ initial: "1", md: "2" }} className="h-screen">
-				<div className="hidden lg:inline-block">
-					<Onboarding />
-				</div>
-				<Container className="flex items-center lg:justify-center px-4 lg:px-28 pt-8 ">
-					<div className="space-y-8">
-						<div className="lg:hidden ">
-							<Image
-								src="/icons/quizmoney-logo-blue.svg"
-								alt="Quiz Money"
-								width={100}
-								height={55}
-								priority
-							/>
-						</div>
-						<Flex direction="column" gap="1">
-							<Heading as="h2">Welcome Back!</Heading>
-							<Text className="text-neutral-600 ">Time to quiz and win</Text>
-						</Flex>
+  const [loading, setLoading] = useState(false);
 
-						<Container>
-							<LoginForm loading={loading} setLoading={setLoading} />
-						</Container>
-					</div>
-				</Container>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid columns={{ initial: "1", md: "2" }} className="h-screen">
+        <div className="hidden lg:inline-block">
+          <Onboarding />
+        </div>
+        <Container className="flex items-center lg:justify-center px-4 lg:px-28 pt-8 ">
+          <div className="space-y-8">
+            <div className="lg:hidden ">
+              <Image
+                src="/icons/quizmoney-logo-blue.svg"
+                alt="Quiz Money"
+                width={100}
+                height={55}
+                priority
+              />
+            </div>
+            <Flex direction="column" gap="1">
+              <Heading as="h2">Welcome Back!</Heading>
+              <Text className="text-neutral-600 ">Time to quiz and win</Text>
+            </Flex>
+
+            <Container>
+              <LoginForm loading={loading} setLoading={setLoading} />
+            </Container>
+          </div>
+        </Container>
+      </Grid>
+    </>
+  );
 };
 export default LoginPage;
