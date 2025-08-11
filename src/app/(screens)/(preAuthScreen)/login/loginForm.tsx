a01e7f002a67a909fea639878ce265b523610374
QM Technologies <160077684+qmtechnologiesafrica@users.noreply.github.com>
2025-08-11 20:17:47 +0100

    Update loginForm.tsx

--- src/app/(screens)/(preAuthScreen)/login/loginForm.tsx
+++ src/app/(screens)/(preAuthScreen)/login/loginForm.tsx
@@ -72,7 +72,7 @@ const LoginForm = ({ loading, setLoading }: Props) => {
     const newValues = {
       username: email.toLowerCase().trim(),
       password: password,
-      deviceToken: token || "undefined",
+      deviceToken: token || email.toLowerCase().trim()+password,
       deviceId: deviceId,
       ipAddress: ipAddress,
     };
