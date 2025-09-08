414a817a19508b29b8829199360ba6b7c3420dd4
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-09-08 23:02:22 +0100

    fix:update

--- src/app/components/referral/TieredReferralProgram.tsx
+++ src/app/components/referral/TieredReferralProgram.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Check, UserRoundPlusIcon, UsersRoundIcon } from "lucide-react";
+import { Check } from "lucide-react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 interface TieredReferralProgramProps {
